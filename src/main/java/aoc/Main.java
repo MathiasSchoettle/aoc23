966cbc965fdc2e399bfa60b8c7667758d9fed247
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-12-02 22:15:28 +0100

    day 1

---
+++ src/main/java/aoc/Main.java
@@ -0,0 +1,9 @@
+package aoc;
+
+import aoc.day.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        new Day1().solve();
+    }
+}
---
+++ src/main/java/aoc/day/Day1.java
@@ -0,0 +1,49 @@
+package aoc.day;
+
+import aoc.AbstractSolver;
+
+import java.util.Map;
+
+public class Day1 extends AbstractSolver {
+    public Day1() {super(1);}
+
+    @Override
+    public Integer puzzle1() {
+        return getLines().stream()
+                .map(s -> s.replaceAll("\\D", ""))
+                .map(s -> s.charAt(0) + "" + s.charAt(s.length() - 1))
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    private static final Map<String, String> VALUES = Map.of(
+            "one", "1",
+            "two", "2",
+            "three", "3",
+            "four", "4",
+            "five", "5",
+            "six", "6",
+            "seven", "7",
+            "eight", "8",
+            "nine", "9"
+    );
+
+    @Override
+    public Integer puzzle2() {
+        return getLines().stream()
+                .map(this::replaceTextNumbers)
+                .map(s -> s.replaceAll("\\D", ""))
+                .map(s -> s.charAt(0) + "" + s.charAt(s.length() - 1))
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    private String replaceTextNumbers(String value) {
+        for (var entry : VALUES.entrySet()) {
+            var replace = entry.getKey() + entry.getValue() + entry.getKey();
+            value = value.replace(entry.getKey(), replace);
+        }
+
+        return value;
+    }
+}
