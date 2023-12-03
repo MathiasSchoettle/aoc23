6151a75645687f4fb9bf4b27cb358f9ecf80ca9f
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-12-03 16:53:57 +0100

    day 3 part 1 refactor

--- src/main/java/aoc/day/Day3.java
+++ src/main/java/aoc/day/Day3.java
@@ -8,54 +8,23 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Day3 extends AbstractSolver {
+    private static final Pattern NUMBERS = Pattern.compile("\\d+");
+    public static final Pattern STAR = Pattern.compile("\\*");
+
     public Day3() {
         super(3);
     }
 
     @Override
     public Object puzzle1() {
-
-        var pattern = Pattern.compile("\\d+");
-        var lines = getLines();
         var sum = 0;
 
-        for (int i = 0; i < lines.size(); i++) {
-            var before = i > 0 ? lines.get(i - 1) : null;
-            var current = lines.get(i);
-            var after = i < lines.size() - 1 ? lines.get(i + 1) : null;
-            var matcher = pattern.matcher(current);
+        for (int i = 0; i < getLines().size(); i++) {
+            var liner = Liner.build(getLines(), i);
 
-            while (matcher.find()) {
-
-                var start = matcher.start();
-                var end = matcher.end() - 1;
-                boolean isPartNumber = false;
-
-                if (start > 0) {
-                    Character c = current.charAt(start - 1);
-                    if (!c.equals('.')) isPartNumber = true;
-                }
-
-                if (end < current.length() - 1) {
-                    Character c = current.charAt(end + 1);
-                    if (!c.equals('.')) isPartNumber = true;
-                }
-
-                var substringStart = start > 0 ? start - 1 : start;
-                var substringEnd = end < current.length() - 1 ? end + 1 : end;
-
-                if (before != null) {
-                    var substring = before.substring(substringStart, substringEnd + 1).replace(".", "");
-                    if (!substring.isEmpty()) isPartNumber = true;
-                }
-
-                if (after != null) {
-                    var substring = after.substring(substringStart, substringEnd + 1).replace(".", "");
-                    if (!substring.isEmpty()) isPartNumber = true;
-                }
-
-                if (isPartNumber) {
-                    sum += Integer.parseInt(matcher.group());
+            for (var match : liner.getMatches()) {
+                if (liner.checkVertical(match) || liner.checkHorizontal(match)) {
+                    sum += match.value();
                 }
             }
         }
@@ -65,16 +34,12 @@ public class Day3 extends AbstractSolver {
 
     @Override
     public Object puzzle2() {
-        var pattern = Pattern.compile("\\d+");
-        var starPattern = Pattern.compile("\\*");
         var lines = getLines();
         var map = new HashMap<Point, List<Integer>>();
 
         for (int i = 0; i < lines.size(); i++) {
-            var before = i > 0 ? lines.get(i - 1) : null;
-            var current = lines.get(i);
-            var after = i < lines.size() - 1 ? lines.get(i + 1) : null;
-            var matcher = pattern.matcher(current);
+            var liner = Liner.build(lines, i);
+            var matcher = NUMBERS.matcher(liner.current);
 
             while (matcher.find()) {
 
@@ -82,26 +47,26 @@ public class Day3 extends AbstractSolver {
                 var end = matcher.end() - 1;
                 var value = Integer.parseInt(matcher.group());
 
-                if (start > 0 && current.charAt(start - 1) == '*') {
+                if (start > 0 && liner.current.charAt(start - 1) == '*') {
                     map.computeIfAbsent(new Point(start - 1, i), point -> new ArrayList<>()).add(value);
                 }
 
-                if (end < current.length() - 1 && current.charAt(end + 1) == '*') {
+                if (end < liner.current.length() - 1 && liner.current.charAt(end + 1) == '*') {
                     map.computeIfAbsent(new Point(end + 1, i), point -> new ArrayList<>()).add(value);
                 }
 
                 var substringStart = start > 0 ? start - 1 : start;
-                var substringEnd = end < current.length() - 1 ? end + 1 : end;
+                var substringEnd = end < liner.current.length() - 1 ? end + 1 : end;
 
-                if (before != null) {
-                    var beforeMatcher = starPattern.matcher(before.substring(substringStart, substringEnd + 1));
+                if (liner.previous != null) {
+                    var beforeMatcher = STAR.matcher(liner.previous.substring(substringStart, substringEnd + 1));
                     while (beforeMatcher.find()) {
                         map.computeIfAbsent(new Point(beforeMatcher.start() + substringStart, i - 1), point -> new ArrayList<>()).add(value);
                     }
                 }
 
-                if (after != null) {
-                    var afterMatcher = starPattern.matcher(after.substring(substringStart, substringEnd + 1));
+                if (liner.next != null) {
+                    var afterMatcher = STAR.matcher(liner.next.substring(substringStart, substringEnd + 1));
                     while (afterMatcher.find()) {
                         map.computeIfAbsent(new Point(afterMatcher.start() + substringStart, i + 1), point -> new ArrayList<>()).add(value);
                     }
---
+++ src/main/java/aoc/day/Liner.java
@@ -0,0 +1,60 @@
+package aoc.day;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Liner {
+    public final String previous;
+    public final String current;
+    public final String next;
+    private static final Pattern NUMBERS = Pattern.compile("\\d+");
+
+    private Liner(String previous, String current, String next) {
+        this.previous = previous;
+        this.current = current;
+        this.next = next;
+    }
+
+    public static Liner build(List<String> lines, int index) {
+        var previous = index > 0 ? lines.get(index - 1) : null;
+        var current = lines.get(index);
+        var next = index < lines.size() - 1 ? lines.get(index + 1) : null;
+
+        return new Liner(previous, current, next);
+    }
+
+    public boolean checkVertical(Match match) {
+        return notDot(match.start - 1) || notDot(match.end);
+    }
+
+    public boolean checkHorizontal(Match match) {
+        return containsSymbol(previous, match) || containsSymbol(next, match);
+    }
+
+    private boolean containsSymbol(String line, Match match) {
+        if (line == null) return false;
+
+        var start = match.start + (match.start > 0 ? -1 : 0);
+        var end = match.end + (match.end < current.length() - 1 ? 1 : 0);
+
+        System.out.println(line.substring(start, end) + " - " + line.matches("^[.]"));
+
+        return !line.substring(start, end).replace(".", "").isEmpty();
+    }
+
+    private boolean notDot(int index) {
+        // out of bounds is like a dot
+        if (index < 0 || index >= current.length()) return false;
+
+        return !Character.valueOf(current.charAt(index)).equals('.');
+    }
+
+    public List<Match> getMatches() {
+        return NUMBERS.matcher(current).results()
+                .map(m -> new Match(m.start(), m.end(), Integer.parseInt(m.group())))
+                .toList();
+    }
+
+    public record Match(int start, int end, int value) {}
+}
