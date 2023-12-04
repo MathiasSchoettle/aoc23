c688c378c34ea0fdbe2ad2bcc251eb24f022ee66
mschoettle <schoettlemathias@gmail.com>
2023-12-04 10:55:45 +0100

    day 4 part 1

---
+++ src/main/java/aoc/day/Day4.java
@@ -0,0 +1,38 @@
+package aoc.day;
+
+import aoc.AbstractSolver;
+
+import java.util.Arrays;
+
+public class Day4 extends AbstractSolver {
+    public Day4() {
+        super(4);
+    }
+
+    @Override
+    public Object puzzle1() {
+        int sum = 0;
+
+        for (var line : getLines()) {
+            String[] full = line.split(":")[1].trim().split("\\|");
+
+            var winners = Arrays.stream(full[0].trim().split(" +"))
+                    .distinct()
+                    .map(Integer::parseInt)
+                    .toList();
+
+            sum += Arrays.stream(full[1].trim().split(" +"))
+                    .map(Integer::parseInt)
+                    .filter(winners::contains)
+                    .reduce(1, (v1,v2) -> v1 * 2) / 2;
+        }
+
+        return sum;
+    }
+
+    @Override
+    public Object puzzle2() {
+
+        return null;
+    }
+}
---
+++ src/test/java/aoc/day/Day4Test.java
@@ -0,0 +1,13 @@
+package aoc.day;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day4Test {
+
+    @Test
+    void puzzle1() {
+        assertEquals(27845, new Day4().puzzle1());
+    }
+}
