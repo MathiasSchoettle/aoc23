31cd49f5e91857c951a32a233e8d0e4c4c46539d
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-12-05 16:32:43 +0100

    day 5 part 1 small fixups

--- src/main/java/aoc/day/Day5.java
+++ src/main/java/aoc/day/Day5.java
@@ -6,13 +6,13 @@ import aoc.day.util.day5.Ranger;
 import java.util.*;
 
 public class Day5 extends AbstractSolver {
-    
+
     public Day5() {
         super(5);
     }
 
     @Override
-    public Object puzzle1() throws Exception {
+    public Object puzzle1() {
         var inputs = splitByEmptyLine();
 
         var seeds = Arrays.stream(inputs.get(0).split(" ")).skip(1).map(Long::parseLong).toList();
