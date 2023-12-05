a1e1d37c08ed28e5b416ee8a3fac566a782dd39f
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-12-05 16:33:09 +0100

    day 5 part 2

--- src/main/java/aoc/day/Day5.java
+++ src/main/java/aoc/day/Day5.java
@@ -34,7 +34,81 @@ public class Day5 extends AbstractSolver {
     }
 
     @Override
-    public Object puzzle2() throws Exception {
-        return null;
+    public Object puzzle2() {
+        var inputs = splitByEmptyLine();
+
+        var seedRanges = Arrays.stream(inputs.get(0).split(" ")).skip(1).map(Long::parseLong).toList();
+        List<Range> ranges = new ArrayList<>();
+
+        for (int i = 0; i < seedRanges.size(); i += 2) {
+            ranges.add(new Range(seedRanges.get(i), seedRanges.get(i + 1)));
+        }
+
+        List<List<Mapper>> mappers = inputs.stream().skip(1).map(block ->
+                Arrays.stream(block.split("\n"))
+                        .skip(1)
+                        .map(line -> {
+                            var arr = line.split(" ");
+                            return new Mapper(Long.parseLong(arr[1]), Long.parseLong(arr[0]), Long.parseLong(arr[2]));
+                        }).toList()).toList();
+
+        for (var mapper : mappers) {
+            ranges = nextRanges(ranges, mapper);
+        }
+
+        return ranges.stream().min(Comparator.comparingLong(o -> o.from)).orElseThrow().from;
+    }
+
+
+    private List<Range> nextRanges(List<Range> ranges, List<Mapper> mappers) {
+
+        // list of ranges which where in range of mappers
+        // store separately and merge with ranges at end to prevent subsequent mappers matching witch an already
+        // mapped region
+        var mappedRanges = new ArrayList<Range>();
+
+        for (var mapper : mappers) {
+
+            var tempList = new ArrayList<Range>();
+
+            for (var range : ranges) {
+
+                if (range.to() < mapper.source || range.from > mapper.to()) {
+                    // completely outside of mapper range
+                    tempList.add(range);
+                } else {
+                    if (mapper.source - range.from > 0) {
+                        // left side of input range is outside of mapper range
+                        // should be handled by following mappers
+                        tempList.add(new Range(range.from, mapper.source - range.from));
+                    }
+
+                    if (range.to() - mapper.to() > 0) {
+                        // right side of input range is outside of mapper range
+                        // should be handled by following mappers
+                        tempList.add(new Range(mapper.to() - 1, range.to() - mapper.to()));
+                    }
+
+                    // if we are here we always have a part of the input range inside the mapper range
+                    // this is the part we need to transform and exclude from following mapper checks
+                    var upper = Math.max(range.from, mapper.source) - mapper.source + mapper.destination;
+                    var lower = Math.min(range.to(), mapper.to()) - mapper.source + mapper.destination;
+                    mappedRanges.add(new Range(upper, lower - upper));
+                }
+            }
+
+            ranges = tempList;
+        }
+
+        mappedRanges.addAll(ranges);
+        return mappedRanges;
+    }
+
+    private record Mapper(long source, long destination, long count) {
+        public long to() {return source + count;}
+    }
+
+    private record Range(long from, long count) {
+        public long to() {return from + count;}
     }
 }
