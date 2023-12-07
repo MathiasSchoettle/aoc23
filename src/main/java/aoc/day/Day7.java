79ae69e3d0c25ca7962e1a98bc281dad816e5c54
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-12-07 13:24:47 +0100

    Day 7 part 1 refactor

--- src/main/java/aoc/day/Day7.java
+++ src/main/java/aoc/day/Day7.java
@@ -2,75 +2,62 @@ package aoc.day;
 
 import aoc.AbstractSolver;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Day7 extends AbstractSolver {
 
-    String input = "32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483\n";
-
     @Override
     public Object puzzle1() {
-        Map<String, Long> map = lines().stream().map(line -> line.split(" "))
-                .collect(Collectors.toMap(arr -> arr[0], arr -> Long.parseLong(arr[1])));
+        var mapped = lines().stream().map(line -> line.split(" "))
+                .collect(Collectors.toMap(arr -> arr[0], arr -> Long.parseLong(arr[1])))
+                .entrySet().stream()
+                .collect(Collectors.toMap(
+                        entry -> strength(entry.getKey()),
+                        Map.Entry::getValue,
+                        (k1, k2) -> k1,
+                        TreeMap::new
+                ));
 
-        Map<Long, Long> collect = map.entrySet().stream()
-                .collect(Collectors.toMap(entry -> toValue(entry.getKey()), Map.Entry::getValue));
+        long i = 1, sum = 0L;
 
-        var sorted = new TreeMap<>(collect);
-
-        int i = 1;
-        long sum = 0L;
-
-        for (var entry : sorted.entrySet()) {
+        for (var entry : mapped.entrySet()) {
             sum += i++ * entry.getValue();
         }
 
         return sum;
     }
 
-    Map<Character, Long> map = Map.ofEntries(
-            Map.entry('A', 14L),
-            Map.entry('K', 13L),
-            Map.entry('Q', 12L),
-            Map.entry('J', 11L),
-            Map.entry('T', 10L),
-            Map.entry('9', 9L),
-            Map.entry('8', 8L),
-            Map.entry('7', 7L),
-            Map.entry('6', 6L),
-            Map.entry('5', 5L),
-            Map.entry('4', 4L),
-            Map.entry('3', 3L),
-            Map.entry('2', 2L)
-    );
+    private long strength(String card) {
 
-    private long toValue(String cards) {
-        Map<String, Long> collect = Arrays.stream(cards.split(""))
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        // Ace (A) is also valid hex
+        var hex = card.replace("A", "E")
+                       .replace("K", "D")
+                       .replace("Q", "C")
+                       .replace("J", "B")
+                       .replace("T", "A");
 
-        long multi = switch (collect.size()) {
-            case 5 -> 1L;
-            case 4 -> 10L;
-            case 3 -> collect.values().stream().max(Comparator.naturalOrder()).get() == 3L ? 1_000L : 100L;
-            case 2 -> collect.values().stream().sorted().findFirst().get() == 2L ? 10_000L : 100_000L;
-            case 1 -> 1_000_000L;
-            default -> throw new RuntimeException("Invalid case for card matching: " + collect.size());
-        } * 10_000_000_000L;
+        var test = Arrays.stream(hex.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.toList()))
+                .values().stream()
+                .map(List::size)
+                .sorted(Comparator.reverseOrder())
+                .map(String::valueOf)
+                .collect(Collectors.joining());
 
-        long value = 0L;
-        long m = 1L;
+        long handType = switch (test) {
+            case "11111" -> 1;
+            case "2111"  -> 2;
+            case "221"   -> 3;
+            case "311"   -> 4;
+            case "32"    -> 5;
+            case "41"    -> 6;
+            case "5"     -> 7;
+            default -> throw new IllegalStateException("Unexpected value: " + test);
+        };
 
-        for (int i = 4; i >= 0; i--) {
-            value += map.get(cards.charAt(i)) * m;
-            m *= 100L;
-        }
-
-        return multi + value;
+        return Long.parseLong(handType + hex, 16);
     }
 
     @Override
