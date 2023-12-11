2fe56220808ff7d78c1bdd216e328973aedd8d12
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-12-11 17:52:53 +0100

    Day 10 part 1 test

---
+++ src/test/java/aoc/day/Day10Test.java
@@ -0,0 +1,17 @@
+package aoc.day;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day10Test {
+
+    @Test
+    void puzzle1() {
+        assertEquals(6599, new Day10().puzzle1());
+    }
+
+    @Test
+    void puzzle2() {
+    }
+}
